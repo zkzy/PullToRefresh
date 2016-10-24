0ce938ffe4a422b8f0bbdb7b779d6a497127fbba
zk <zhangkai8834@163.com>
2016-10-24 14:43:04 +0800

    兼容GridLayoutManager

--- app/src/main/java/com/example/hasee/pulltorefresh/MainActivity.java
+++ app/src/main/java/com/example/hasee/pulltorefresh/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.hasee.pulltorefresh;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -13,25 +14,30 @@ import com.example.library.RefreshAndLoadMoreListener;
 
 import java.util.ArrayList;
 
+import it.gmariotti.recyclerview.itemanimator.SlideInOutBottomItemAnimator;
+
+import static com.example.hasee.pulltorefresh.R.id.swipeRefreshLayout;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private MainAdapter mainAdapter;
-    Handler handler=new Handler(){
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             mainAdapter.finishLode(BaseRecycleViewAdapter.Status.STATUS_NOMAL);
             mainAdapter.finishRefresh();
-            if(msg.what==0){
+            if (msg.what == 0) {
                 mainAdapter.addLoadMoreItmes(initData("加载更多"));
             }
-            if(msg.what==1){
+            if (msg.what == 1) {
                 mainAdapter.addRefreshItmes(initData("刷新"));
             }
 
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,30 +46,33 @@ public class MainActivity extends AppCompatActivity {
         mainAdapter.addLoadMoreItmes(initData(""));
     }
 
-    private ArrayList<String>  initData(String data) {
+    private ArrayList<String> initData(String data) {
         ArrayList<String> list = new ArrayList<>();
-        for (char i = 'A'; i <='Z' ; i++) {
-            list.add(String.valueOf(i)+data);
+        for (char i = 'A'; i <= 'Z'; i++) {
+            list.add(String.valueOf(i) + data);
         }
-      return list;
+        return list;
     }
 
     private void initView() {
-        mRecyclerView= (RecyclerView) findViewById(R.id.recycleview);
-        mSwipeRefreshLayout= (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleview);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(swipeRefreshLayout);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mainAdapter = new MainAdapter(this,mRecyclerView,mSwipeRefreshLayout);
+        mainAdapter = new MainAdapter(this, mRecyclerView, mSwipeRefreshLayout);
         mRecyclerView.setAdapter(mainAdapter);
+        mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light, android.R.color.holo_orange_light, android.R.color.holo_green_light);
+        mRecyclerView.setItemAnimator(new SlideInOutBottomItemAnimator(mRecyclerView));
         mainAdapter.setOnRefreshAndLoadMoreListener(new RefreshAndLoadMoreListener() {
             @Override
             public void onLoadingMoreData() {
-                handler.sendEmptyMessageDelayed(0,3000);
+                handler.sendEmptyMessageDelayed(0, 3000);
 
             }
 
             @Override
             public void onRefreshingData() {
-                handler.sendEmptyMessageDelayed(1,3000);
+                handler.sendEmptyMessageDelayed(1, 3000);
             }
         });
     }
--- library/src/main/java/com/example/library/BaseRecycleViewAdapter.java
+++ library/src/main/java/com/example/library/BaseRecycleViewAdapter.java
@@ -1,10 +1,10 @@
 package com.example.library;
 
 import android.content.Context;
-
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,11 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import static android.support.v7.widget.RecyclerView.*;
+import static android.support.v7.widget.RecyclerView.LayoutManager;
+import static android.support.v7.widget.RecyclerView.OnScrollListener;
+import static android.support.v7.widget.RecyclerView.VISIBLE;
+import static android.support.v7.widget.RecyclerView.ViewHolder;
 
 /**
  * Created by hasee on 2016/10/21.
@@ -28,24 +31,8 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
     public final Context mContext;
     private final RecyclerView mRecyclerView;
     private SwipeRefreshLayout mSwipeRefreshLayout;
+
     private static final String TAG = "BaseRecycleViewAdapter";
-
-    private String STATUS_NO_MORE = "没有更多了";
-    private String STATUS_NOMAL = "正在加载...";
-    private String STATUS_NO_NET = "没有网络了";
-    private LoadingHolder loadingHolder;
-
-    public enum Status {
-        STATUS_NO_MORE, STATUS_NOMAL, STATUS_NO_NET
-    }
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-        }
-    };
 
     /**
      * 正常的条目
@@ -60,16 +47,91 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
      * 是否正在刷新
      */
     private boolean isLoadingNow;
+    private String STATUS_NO_MORE = "没有更多了";
+    private String STATUS_NOMAL = "正在加载...";
+    private String STATUS_NO_NET = "没有网络了";
+
+    private int mLoadingTextColor;
+
+
+    public enum Status {
+        STATUS_NO_MORE, STATUS_NOMAL, STATUS_NO_NET
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch ((Status)msg.obj){
+                case STATUS_NO_MORE:
+                    Toast.makeText(mContext, ""+STATUS_NO_MORE, Toast.LENGTH_SHORT).show();
+                    break;
+                case STATUS_NO_NET:
+                    Toast.makeText(mContext, ""+STATUS_NO_NET, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+
+
+    public BaseRecycleViewAdapter(Context context, RecyclerView recyclerView, SwipeRefreshLayout swipeRefreshLayout) {
+        this.mContext = context;
+        this.mRecyclerView = recyclerView;
+        mSwipeRefreshLayout = swipeRefreshLayout;
+        this.mDataRes = new ArrayList<>();
+
+        if (recyclerView != null) {
+            this.mRecyclerView.addOnScrollListener(mOnScrollListener);
+        }
+
+        if (mSwipeRefreshLayout != null) {
+            this.mSwipeRefreshLayout.setOnRefreshListener(this);
+        }
+
+    }
+    /**
+     * 特殊的条目占几个位置
+     * @param recyclerView
+     */
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        LayoutManager layoutManager=recyclerView.getLayoutManager();
+        if(layoutManager instanceof GridLayoutManager){
+            final GridLayoutManager gridLayoutManager= (GridLayoutManager) layoutManager;
+            gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    int viewType=getItemViewType(position);
+                    return viewType==LOADING_TYPE ? gridLayoutManager.getSpanCount() : 1;
+
+                }
+            });
+        }
+    }
 
     OnScrollListener mOnScrollListener = new OnScrollListener() {
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
             super.onScrolled(recyclerView, dx, dy);
-            LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
             if (dy > 0) {
-                int firstVisiblePosition = linearLayoutManager.findFirstVisibleItemPosition();
-                int totalItemCount = linearLayoutManager.getItemCount();
-                int visibleItemCount = linearLayoutManager.getChildCount();
+                LayoutManager layoutManager =recyclerView.getLayoutManager();
+                int firstVisiblePosition=-1;
+                int totalItemCount=-1;
+                int visibleItemCount=-1;
+                if(layoutManager instanceof LinearLayoutManager){
+                    LinearLayoutManager linearLayoutManager= (LinearLayoutManager) layoutManager;
+                    firstVisiblePosition = linearLayoutManager.findFirstVisibleItemPosition();
+                    totalItemCount = linearLayoutManager.getItemCount();
+                    visibleItemCount = linearLayoutManager.getChildCount();
+                }else if(layoutManager instanceof GridLayoutManager){
+                    GridLayoutManager gridLayoutManager= (GridLayoutManager) layoutManager;
+                    firstVisiblePosition = gridLayoutManager.findFirstVisibleItemPosition();
+                    totalItemCount = gridLayoutManager.getItemCount();
+                    visibleItemCount = gridLayoutManager.getChildCount();
+                }
+
                 Log.e(TAG, "onScrolled:firstVisiblePosition= " + firstVisiblePosition
                         + "visibleItemCount=" + visibleItemCount + "totalItemCount=" + totalItemCount);
                 if (!isLoadingNow && (firstVisiblePosition + visibleItemCount) >= totalItemCount) {
@@ -84,6 +146,9 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
     };
     private RefreshAndLoadMoreListener mRefreshAndLoadMoreListener;
 
+    /**
+     * 正在刷新
+     */
     @Override
     public void onRefresh() {
         if (mRefreshAndLoadMoreListener != null) {
@@ -91,7 +156,9 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
         }
     }
 
-
+    /**
+     * 触发开始加载更多，显示最后一个刷新条目
+     */
     private void stratLoadingMore() {
         if (!isLoadingNow || mDataRes.size() < 1) return;
         int position = getLastItemPosition();
@@ -99,6 +166,9 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
         notifyItemInserted(position + 1);
     }
 
+    /**
+     * 删除刷新，删除刷新时候添加的数据
+     */
     private void finishLoadingMore() {
         if (!isLoadingNow) return;
         int position = getLastItemPosition();
@@ -108,34 +178,35 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
 
     }
 
+    /**
+     * 获取最后一个条目的位置
+     * @return
+     */
     private int getLastItemPosition() {
         return isLoadingNow ? getItemCount() - 1 : RecyclerView.NO_POSITION;
     }
 
+    /**
+     * 提供给外界的方法，停止加载更多
+     * @param status
+     */
     public void finishLode(Status status) {
+        Message msg=Message.obtain(handler);
+        msg.obj=status;
+        handler.handleMessage(msg);
         finishLoadingMore();
     }
 
+    /**
+     * 停止刷新
+     */
     public void finishRefresh() {
         if (mSwipeRefreshLayout != null) {
             mSwipeRefreshLayout.setRefreshing(false);
         }
     }
 
-    public BaseRecycleViewAdapter(Context context, RecyclerView recyclerView, SwipeRefreshLayout swipeRefreshLayout) {
-        this.mContext = context;
-        this.mRecyclerView = recyclerView;
-        mSwipeRefreshLayout = swipeRefreshLayout;
-        this.mDataRes = new ArrayList<>();
-        if (recyclerView != null) {
-            this.mRecyclerView.addOnScrollListener(mOnScrollListener);
-        }
-        if (mSwipeRefreshLayout != null) {
-            this.mSwipeRefreshLayout.setOnRefreshListener(this);
-            swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light, android.R.color.holo_orange_light, android.R.color.holo_green_light);
-        }
 
-    }
 
     @Override
     public BaseRecycleViewAdapter.BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -166,7 +237,7 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
     }
 
     private void bindLoadingViewHolder(BaseViewHolder holder, int position) {
-        loadingHolder = (LoadingHolder) holder;
+        LoadingHolder loadingHolder = (LoadingHolder) holder;
         loadingHolder.mProgressBar.setVisibility(VISIBLE);
         loadingHolder.mTevLoad.setText(STATUS_NOMAL);
         loadingHolder.mTevLoad.setVisibility(VISIBLE);
@@ -184,7 +255,10 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
         return mDataRes.size();
     }
 
-
+    /***
+     * 设置加载更多，刷新的监听
+     * @param refreshAndLoadMoreListener
+     */
     public void setOnRefreshAndLoadMoreListener(RefreshAndLoadMoreListener refreshAndLoadMoreListener) {
         mRefreshAndLoadMoreListener = refreshAndLoadMoreListener;
     }
@@ -222,6 +296,20 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
         }
     }
 
+    public void setLoadingTextColor(int color){
+        mLoadingTextColor = color;
+    }
+
+    public void setRefreshEnabled(boolean enabled){
+        if(mSwipeRefreshLayout!=null&&!enabled){
+            mSwipeRefreshLayout.setOnRefreshListener(null);
+        }
+    }
+    public void setLoadMoreEnabled(boolean enabled){
+        if(mRecyclerView!=null&&!enabled){
+            mRecyclerView.removeOnScrollListener(mOnScrollListener);
+        }
+    }
 
     /**
      * 清空数据
@@ -245,6 +333,7 @@ public abstract class BaseRecycleViewAdapter<T> extends RecyclerView.Adapter<Bas
             super(itemView);
             mProgressBar = (ProgressBar) itemView.findViewById(R.id.progressbar);
             mTevLoad = (TextView) itemView.findViewById(R.id.tev_load);
+            mTevLoad.setTextColor(mContext.getResources().getColor(mLoadingTextColor));
         }
     }
 }
